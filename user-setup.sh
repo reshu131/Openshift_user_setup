6a3e60005641b1ef6e71f02e8ac4676836ff2df9
reshu131 <41335487+reshu131@users.noreply.github.com>
2020-07-19 11:41:36 +0530

    Update user-setup.sh

--- user-setup.sh
+++ user-setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 touch /etc/origin/master/htpasswd
-htpasswd /etc/origin/master/htpasswd ris ris
+htpasswd -b /etc/origin/master/htpasswd ris ris
 oc adm policy add-role-to-user admin ris
